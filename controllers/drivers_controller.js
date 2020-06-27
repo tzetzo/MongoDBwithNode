f617326604c88b0693345a84304a199641889fde
tzetzo <tzvetanmarinov@yahoo.com>
2020-06-27 13:43:46 +0300

    endpoint and handler for creating driver document

--- controllers/drivers_controller.js
+++ controllers/drivers_controller.js
@@ -1,6 +1,13 @@
+const Driver = require('../models/driver');
+
 module.exports = {
   greeting(req, res) {
     res.send({ hi: "there" });
-  }
+  },
+  create(req, res) {
+    Driver.create(req.body).then((dr) => {
+      res.send(dr);
+    })
 
+  }
 }
