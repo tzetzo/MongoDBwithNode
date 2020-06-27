b4341a8802acd073bd05a73c31966b05940e391a
tzetzo <tzvetanmarinov@yahoo.com>
2020-06-27 13:45:22 +0300

    connect to local MongoDB & use body-parser as express middleware

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 const routes = require("./routes/routes");
 
 const app = express();
+
+if (process.env.NODE_ENV !== "test") {
+  mongoose.connect(
+    "mongodb://localhost:27017/muber",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  );
+}
+
+app.use(bodyParser.json()); //put above the routes!
 
 routes(app);
 
