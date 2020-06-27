fb2d2691fa8bc04dc1f52b3f475c50bd02bbc0a6
tzetzo <tzvetanmarinov@yahoo.com>
2020-06-27 16:23:52 +0300

    custom middleware to handle errors thrown in our endpoints

--- controllers/drivers_controller.js
+++ controllers/drivers_controller.js
@@ -1,13 +1,14 @@
-const Driver = require('../models/driver');
+const Driver = require("../models/driver");
 
 module.exports = {
   greeting(req, res) {
     res.send({ hi: "there" });
   },
-  create(req, res) {
-    Driver.create(req.body).then((dr) => {
-      res.send(dr);
-    })
-
+  create(req, res, next) {
+    Driver.create(req.body)
+      .then(dr => {
+        res.send(dr);
+      })
+      .catch(next); //transfers controll over to the error handling middleware(see app.js)
   }
-}
+};
