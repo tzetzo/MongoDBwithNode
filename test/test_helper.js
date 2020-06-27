c8ab2c75da695bc31c2ec3c88731cb42726e8ead
tzetzo <tzvetanmarinov@yahoo.com>
2020-06-27 13:46:36 +0300

    setup test environment - separate DB for testing

---
+++ test/test_helper.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+console.log('test helper un')
+before(done => {
+  // guarantees test will be run after mongoose connects to MongoDB; run just once!
+  mongoose.connect(
+    "mongodb://localhost:27017/muber_test",
+    { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }
+  );
+  mongoose.connection
+    .once("open", () => {
+      done();
+    })
+    .on("error", error => {
+      console.warn("Warning", error);
+    });
+});
+
+beforeEach(done => {
+  const { drivers } = mongoose.connection.collections; //mongoose lowercases the names of the collections!
+
+  drivers
+    .drop()
+    .then(() => done())
+    .catch(() => done());
+});
