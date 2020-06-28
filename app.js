598d20e022933b548556fd4de43d2474325e3949
tzetzo <tzvetanmarinov@yahoo.com>
2020-06-28 09:53:55 +0300

    no message

--- app.js
+++ app.js
@@ -8,12 +8,17 @@ const app = express();
 if (process.env.NODE_ENV !== "test") {
   mongoose.connect(
     "mongodb://localhost:27017/muber",
-    { useNewUrlParser: true, useUnifiedTopology: true }
+    { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }
   );
 }
 
 app.use(bodyParser.json()); //put above the routes!
 
 routes(app);
+
+//middleware to catch errors in our endpoints
+app.use((err, req, res, next) => {
+  res.status(422).send({ error: err.message });
+});
 
 module.exports = app;
